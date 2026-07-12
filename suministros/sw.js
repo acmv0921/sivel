4c299a695b1543ba02b960f8a3a136b586611b54
acmv0921 <acmv0921@gmail.com>
2026-07-12 12:41:52 -0500

    Suministros: aviso de actualización PWA disponible

--- suministros/sw.js
+++ suministros/sw.js
@@ -6,8 +6,13 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(PRECACHE))
-      .then(() => self.skipWaiting())
   );
+});
+
+self.addEventListener('message', event => {
+  if (event.data === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener('activate', event => {
