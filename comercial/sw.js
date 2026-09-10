a50baa8026fca8f714077450267451b307f2d13d
acmv0921 <acmv0921@gmail.com>
2026-09-09 20:38:51 -0500

    Update cache name to 'sivil-cache-v33'

--- comercial/sw.js
+++ comercial/sw.js
@@ -1,6 +1,6 @@
 // SIVIL PWA Service Worker v2.0
 // POSTEC DE OCCIDENTE S.A.S.
-const CACHE_NAME = 'sivil-cache-v32';
+const CACHE_NAME = 'sivil-cache-v33';
 const PRECACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 // INSTALL — cachear recursos base
